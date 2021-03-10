2cc77046a0e8004e812799d786402614ae42c1b2
gabalja <80046476+gabalja@users.noreply.github.com>
2021-03-10 22:16:06 +0900

    Create 2581_소수.cpp

---
+++ 1000-9999/2581_소수.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  int m,n;
+  scanf("%d",&m);
+  scanf("%d",&n);
+  int nums[10001]={0,};
+  for(int j=2;j<10001;j++)
+  {
+    nums[j]=j;
+  }
+  for(int k=2;k<10001;k++)  // 에라토스테네스의 체
+  {
+    if(nums[k]==0) continue;
+    for(int l=2*k;l<10001;l+=k)
+    {
+      nums[l]=0;
+    }
+  }
+  int sum=0;
+  int small=0;
+  for(int h=n;h>=m;h--) // 최소의 소수와 소수들의 합을 찾음
+  {
+    if(nums[h]!=0)
+    {
+      sum+=nums[h];
+      small=nums[h];
+    }
+  }
+  if(sum==0)
+  {
+    printf("%d",-1);
+  }
+  else
+  {
+    printf("%d\n",sum);
+    printf("%d",small);
+  }
+}
